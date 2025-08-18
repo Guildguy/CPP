a41252b450f8bb9abddcbd6f23f834357077ade8
Guildguy <fabiolljunior23@gmail.com>
2025-08-18 17:00:23 -0300

    feat(displayPhonebook): added a function that print out a phone list

---
+++ ex01/contacts.cpp
@@ -0,0 +1,24 @@
+#include "contacts.hpp"
+
+void	displayPhonebook(void)
+{
+	std::cout << "---------------------------------------------" << std::endl;
+	std::cout << "|  index:  |   name:  |   last:  |   nick:  |" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
+	std::cout << "|          |          |          |          |" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
+	std::cout << "|          |          |          |          |" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
+	std::cout << "|          |          |          |          |" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
+	std::cout << "|          |          |          |          |" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
+	std::cout << "|          |          |          |          |" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
+	std::cout << "|          |          |          |          |" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
+	std::cout << "|          |          |          |          |" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
+	std::cout << "|          |          |          |          |" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
+}
