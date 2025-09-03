a9d2437361dd318df6a49571400a73d333133a5a
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-09-03 18:51:25 -0300

    update(build): started the Contact class created private: where we store the personal info from the contact list, in public: added constructor n' destructor, also, added the getter n' setter methods

---
+++ 0cpp/ex01/Contacts.hpp
@@ -0,0 +1,38 @@
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <iostream>
+# include <string>
+
+void	displayPhonebook(void);
+
+class	Contact
+{
+	private: //all below is atribute
+		std::string	FirstName;
+		std::string	LastName;
+		std::string	NickName;
+		std::string	PhoneNumber;
+		std::string	DarkestSecret;
+	
+	public: //the way we access the private is w/ public
+		Contact(); //constructor
+		~Contact(); //destructor
+
+//		getter function declaration
+		std::string	GetFirstName();
+		std::string	GetLastName();
+		std::string	GetNickName();
+		std::string	GetPhoneNumber();
+		std::string	GetDarkestSecret();
+
+//		setter function declaration
+		void	SetFirstName(const std::string& SetFirstName);
+		void	SetLastName(const std::string& SetLastName);
+		void	SetNickName(const std::string& SetNickName);
+		void	SetPhoneNumber(const std::string& SetPhoneNumber);
+		void	SetDarkestSecret(const std::string& SetDarkestSecret);
+};
+
+
+#endif
