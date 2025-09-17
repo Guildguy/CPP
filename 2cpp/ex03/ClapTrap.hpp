cf7054ce9661dd0b9be4bc951ec41ca3b9fde40d
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-09-17 15:01:06 -0300

    feat(hpp): class struct done n' another files created

---
+++ 2cpp/ex03/ClapTrap.hpp
@@ -0,0 +1,27 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class ClapTrap
+{
+	private:
+		unsigned int	HP;
+		unsigned int	EP;
+		std::string		ATK;
+
+	public:
+		ClapTrap();
+		~ClapTrap();
+
+		ClapTrap(const ClapTrap &copy);
+
+
+		void	Attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+};
+
+std::ostream&	operator<<(std::ostream& os, const ClapTrap& ClapTrap);
+
+#endif
