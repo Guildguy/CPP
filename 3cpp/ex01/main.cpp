7a6502a83e966db596f6c7cf2789945602693034
Guildguy <fabiolljunior23@gmail.com>
2025-09-19 17:48:30 -0300

    update(test): added new tests to the main

--- 3cpp/ex01/main.cpp
+++ 3cpp/ex01/main.cpp
@@ -5,11 +5,17 @@ int	main(void)
 	ClapTrap	Enemy("Sephiroth");
 	ScavTrap	Person;
 
+	std::cout << Enemy << std::endl;
+	std::cout << Person << std::endl;
 	Enemy.Attack(Person.getName());
-	Person.takeDamage(Enemy.getAttack());
+	if (Enemy.getHP() > 0)
+		Person.takeDamage(Enemy.getAttack());
 	Person.Attack(Enemy.getName());
-	Enemy.takeDamage(Person.getAttack());
+	if (Person.getHP() > 0)
+		Enemy.takeDamage(Person.getAttack());
 	Person.beRepaired(100);
 	Person.guardGate();
+	std::cout << Enemy << std::endl;
+	std::cout << Person << std::endl;
 	return (0);
 }
