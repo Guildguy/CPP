83b88106e009515af3be796673dbefd81f32d932
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-12-07 21:35:02 -0300

    fix(forms): include Bureaucrat.hpp in form header files for consistency, added canonnical ortodoxical form to <ShrubberyCreationForm> header

--- 5cpp/ex02/PresidentialPardonForm.hpp
+++ 5cpp/ex02/PresidentialPardonForm.hpp
@@ -1,6 +1,7 @@
 #ifndef PRESIDENTIALPARDONFORM_HPP
 #define PRESIDENTIALPARDONFORM_HPP
 
+#include "Bureaucrat.hpp"
 #include "AForm.hpp"
 
 #endif
--- 5cpp/ex02/ShrubberyCreationForm.hpp
+++ 5cpp/ex02/ShrubberyCreationForm.hpp
@@ -1,6 +1,23 @@
 #ifndef SHRUBBERYCREATIONFORM_HPP
 #define SHRUBBERYCREATIONFORM_HPP
 
+#include "Bureaucrat.hpp"
 #include "AForm.hpp"
+
+class	AForm;
+
+class	ShrubberyCreationForm
+{
+	private:
+		std::string	target;
+	public:
+		//CANONNICAL ORTODOXICAL FORM
+		ShrubberyCreationForm();
+		ShrubberyCreationForm(const std::string target);
+		ShrubberyCreationForm(const ShrubberyCreationForm& copy);
+		ShrubberyCreationForm&	operator=(const ShrubberyCreationForm& form);
+		~ShrubberyCreationForm();
+};
+
 
 #endif
