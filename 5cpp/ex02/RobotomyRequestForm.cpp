e714f7ed48dbe5631973a54c5fe84e6ca2de3dfc
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-12-07 22:46:22 -0300

    feat(RobotomyRequestForm): implement exec method for robotomy process with random success

--- 5cpp/ex02/RobotomyRequestForm.cpp
+++ 5cpp/ex02/RobotomyRequestForm.cpp
@@ -1,3 +1,51 @@
 #include "AForm.hpp"
 #include "Bureaucrat.hpp"
 #include "RobotomyRequestForm.hpp"
+
+//CANONNICAL ORTODOXICAL FORM
+RobotomyRequestForm::RobotomyRequestForm() : 
+AForm("RobotomyRequestForm", 72, 45),
+target("default") {}
+
+RobotomyRequestForm::RobotomyRequestForm(const std::string target) : 
+AForm("RobotomyRequestForm", 72, 45), 
+target(target) {}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm& copy) :
+AForm(copy), 
+target(copy.target) {}
+
+RobotomyRequestForm& RobotomyRequestForm::operator=(const RobotomyRequestForm& form)
+{
+    if (this != &form)
+	{
+		AForm::operator=(form);
+        this->target = form.target;
+	}
+    return (*this);
+}
+
+RobotomyRequestForm::~RobotomyRequestForm() {}
+
+void RobotomyRequestForm::exec() const {
+    
+    static bool rand_exist = false;
+
+    // seed the random generator
+    if (!rand_exist) {
+        std::srand(std::time(0));
+        rand_exist = true;
+    }
+
+    // get 50% chance
+    if (std::rand() % 2 == 0) {
+        std::cout << "SKREEEEEEEEE!\n"
+                  << target << " has been robotomized successfully."
+                  << std::endl;
+    }
+    else {
+        std::cout << "VRRRRR-ZZZZT-POP...\n"
+                  << target << "'s robotomy has failed!"
+                  << std::endl;
+    }
+}
--- 5cpp/ex02/RobotomyRequestForm.hpp
+++ 5cpp/ex02/RobotomyRequestForm.hpp
@@ -3,5 +3,21 @@
 
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
+#include <cstdlib>
+#include <ctime>
+
+class	RobotomyRequestForm : public AForm
+{
+	private:
+		std::string	target;
+	protected:
+		void	exec() const;
+	public:
+		RobotomyRequestForm();
+		RobotomyRequestForm(const std::string target);
+		RobotomyRequestForm(const RobotomyRequestForm& copy);
+		RobotomyRequestForm& operator=(const RobotomyRequestForm& form);
+		~RobotomyRequestForm();
+};
 
 #endif
