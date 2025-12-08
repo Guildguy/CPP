bfe0c34156a70e9e6bf30890d4f5306638e585cc
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-12-07 22:52:47 -0300

    feat(PresidentialPardonForm): implement canonical form methods and exec function

--- 5cpp/ex02/PresidentialPardonForm.cpp
+++ 5cpp/ex02/PresidentialPardonForm.cpp
@@ -1,3 +1,34 @@
 #include "AForm.hpp"
 #include "Bureaucrat.hpp"
 #include "PresidentialPardonForm.hpp"
+
+//CANONNICAL ORTODOXICAL FORM
+PresidentialPardonForm::PresidentialPardonForm() : 
+AForm("PresidentialPardonForm", 25, 5),
+target("default") {}
+
+PresidentialPardonForm::PresidentialPardonForm(const std::string target) : 
+AForm("PresidentialPardonForm", 25, 5), 
+target(target) {}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& copy) :
+AForm(copy), 
+target(copy.target) {}
+
+PresidentialPardonForm& PresidentialPardonForm::operator=(const PresidentialPardonForm& form)
+{
+    if (this != &form)
+	{
+		AForm::operator=(form);
+        this->target = form.target;
+	}
+    return (*this);
+}
+
+PresidentialPardonForm::~PresidentialPardonForm() {}
+
+void PresidentialPardonForm::exec() const
+{
+    std::cout << "PresidentialPardonForm: Informs that " << target
+              << " has been pardoned by Zaphod Beeblebrox." << std::endl;
+}
