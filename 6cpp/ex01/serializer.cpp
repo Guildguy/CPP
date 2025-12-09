2faa3052dc094b748b8ce32f0f7f01c0172a51ce
fabio jr <fabiolljunior23@gmail.com>
2025-12-09 15:41:35 -0300

    feat(serializer): implement serialize and deserialize methods for Data type

--- 6cpp/ex01/serializer.cpp
+++ 6cpp/ex01/serializer.cpp
@@ -14,3 +14,15 @@ Serializer&	Serializer::operator=(const Serializer& serial)
 }
 
 Serializer::~Serializer() {}
+
+//Reinterpret the bits of the memory address as an integer.
+uintptr_t	Serializer::serialize(Data* ptr)
+{
+	return (reinterpret_cast<uintptr_t>(ptr));
+}
+
+// Take the integer and say "this here is a memory address of Data"
+Data*	Serializer::deserialize(uintptr_t raw)
+{
+	return (reinterpret_cast<Data*>(raw));
+}
