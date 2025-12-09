aeac7371e2c9c85649354c1d6f40a3166ee9526e
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-12-09 11:39:45 -0300

    feat: update ScalarConverter and Makefile for improved conversion handling

--- 6cpp/ex00/ScalarConverter.cpp
+++ 6cpp/ex00/ScalarConverter.cpp
@@ -25,6 +25,45 @@ void	ScalarConverter::convert(std::string& literal)
 	}
 	else
 	{
-		double	dConvert = strtod(&literal);
+		char	*end;
+		double	dConvert = std::strtod(literal.c_str(), &end);
+
+		if (*end != '\0' && std::string(end) != "f")
+		{
+			std::cout << "char: impossible" << std::endl;
+			std::cout << "int: impossible" << std::endl;
+			std::cout << "float: impossible" << std::endl;
+			std::cout << "double: impossible" << std::endl;
+			return ;
+		}
+
+		std::cout << "char: ";
+		if (std::isnan(dConvert) || std::isinf(dConvert) || dConvert < 0 || dConvert > 127)
+			std::cout << "impossible";
+		else if (!std::isprint(static_cast<char>(dConvert)))
+			std::cout << "Non displayable";
+		else
+			std::cout << "'" << static_cast<char>(dConvert) << "'";
+		std::cout << std::endl;
+
+		std::cout << "int: ";
+		if (std::isnan(dConvert) || std::isinf(dConvert) || 
+			dConvert < std::numeric_limits<int>::min() || dConvert > std::numeric_limits<int>::max())
+			std::cout << "impossible";
+		else
+			std::cout << static_cast<int>(dConvert);
+		std::cout << std::endl;
+
+		std::cout << "float: " << static_cast<float>(dConvert);
+		if (dConvert - static_cast<int>(dConvert) == 0) 
+			std::cout << ".0f"; 
+		else 
+			std::cout << "f";
+		std::cout << std::endl;
+
+		std::cout << "double: " << dConvert;
+		if (dConvert - static_cast<int>(dConvert) == 0) 
+			std::cout << ".0";
+		std::cout << std::endl;
 	}
 }
--- 6cpp/ex00/main.cpp
+++ 6cpp/ex00/main.cpp
@@ -2,9 +2,15 @@
 
 int	main(int ac, char **av)
 {
+	std::string	input;
+
 	if ( ac == 2)
 	{
-		if (!av[1].startswith(""))
-			//erro
+		input = av[1];
+		ScalarConverter::convert(input);
 	}
+	else
+		std::cout << "Usage: ./ScalarConverter <literal>\n";
+	
+		return (0);
 }
