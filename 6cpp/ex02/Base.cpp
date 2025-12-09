c449987c04c64e43aaeb69c9aaf2754e05e4ca4d
Guildguy <fabiolljunior23@gmail.com>
2025-12-09 19:02:04 -0300

    feat: implement generate function for random Base class instantiation and add identify functions

--- 6cpp/ex02/Base.cpp
+++ 6cpp/ex02/Base.cpp
@@ -3,4 +3,35 @@
 #include "B.hpp"
 #include "C.hpp"
 
+Base*	generate(void)
+{
+	static bool	rand_exist = false;
 
+	if (!rand_exist)
+	{
+		std::srand(std::time(0));
+		rand_exist = true;
+	}
+	int random = std::rand() % 3;
+
+	switch (random) {
+		case 0:
+			return (new A());
+		case 1:
+			return (new B());
+		case 2:
+			return (new C());
+		default:
+			return (NULL);
+	}
+}
+
+void	indentify(Base* p)
+{
+	//std::string	ptr = 
+}
+
+void	indentify(Base& p)
+{
+	
+}
